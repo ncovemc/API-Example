86998b7880e3e8572119962a9b0b2a05d1622bdd
herywinarto <hery.winarto@binus.ac.id>
2020-04-12 12:26:30 +0700

    Create GIFSearch.py

---
+++ GIFSearch.py
@@ -0,0 +1,30 @@
+
+import requests, json
+
+### FROM https://danbooru.donmai.us/ ###
+
+api_key= "INSERT API KEY HERE"
+
+## GET API KEY FROM LINE ID: hertot ###
+
+def failOverAPI():
+    try:
+        result = requests.get("https://api.boteater.xyz",timeout=0.5)
+        if result.status_code == 200:
+            return "https://api.boteater.xyz"
+        else:
+            return "https://api.boteater.us"
+    except:
+        return "https://api.boteater.us"
+    
+
+def search():
+    if api_key == "INSERT API KEY HERE":
+        print("GET API KEY FROM LINE ID: hertot")
+        raise Exception("Wrong API Key")
+    search = input("Search query: ") # example anime
+    result = json.loads(requests.get(failOverAPI()+"/gif_search?search="+search+"&auth="+api_key).text)
+    for img in result["result"]:
+        print("Link Gif: "+img)
+
+search()
