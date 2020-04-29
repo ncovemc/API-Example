12a082994249a48a5dcc3518fbe40ce9076cab4d
herywinarto <hery.winarto@binus.ac.id>
2020-04-29 16:50:46 +0700

    Create tokenPool.py

---
+++ tokenPool.py
@@ -0,0 +1,34 @@
+import requests, json
+
+
+
+def getNewToken(user):
+    while True:
+        result = json.loads(requests.get("https://pool.boteater.xyz/getToken?user="+user).text)
+        if result["status"] == 200:
+            print("[ New Token ] "+result["result"])
+            return result["result"]
+        else:
+            if "please wait" in result["reason"]:
+                sleeping = int(result["reason"].split(" ")[2])
+                print("[ Limitation ] Waiting {} seconds...".format(sleeping))
+                time.sleep(sleeping)
+            else:
+                raise Exception("[ Token Pool ] Unknown error")
+        time.sleep(1)
+
+def getCurentToken(user):
+    result = json.loads(requests.get("https://pool.boteater.xyz/myToken?user="+user).text)
+    if result["status"] == 200:
+        return result["result"]
+    else:
+        raise Exception("[ Token Pool ] Unknown error")
+
+#example login (linepy)
+client = LINE(getNewToken("usernya"), appName='IOS/t9.18.1/tiPhone OS/t11.2.5')
+
+
+
+#NOTE: Token will change every restart bot ( so if bot ban/limit just restart bot to get new fresh token )
+#LIMITATION: 1 get per 5 minutes
+#ONE USER API FOR 1 ACCOUNT ( SO IF HAVE 3 BOT TOTAL U NEED 3 USER API )
