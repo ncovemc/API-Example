e7af622cc1ca1e362a7d5d1ac25d9100511e29bc
herywinarto <hery.winarto@binus.ac.id>
2020-05-26 20:53:19 +0700

    Update JooxMusic.py
    update

--- JooxMusic.py
+++ JooxMusic.py
@@ -23,11 +23,17 @@ def search():
         raise Exception("Wrong API Key")
     search = input("Search query: ") #example chainsmokers
     result = json.loads(requests.get(failOverAPI()+"/joox?search="+search+"&auth="+api_key).text)
-    for music in result["result"]:
-        print("")
-        print("Artis: "+music["artist"])
-        print("Title: "+music["title"])
-        print("Link Audio: "+music["stream-url"])
-        print("Link Image: "+music["image"])
+    for num in range(len(result["result"])):
+        data = result["result"][num]
+        print("{}. {} || {}".format(num+1,data["title"],data["artist"]))
+    select = int(input("Number of song: "))
+    r = json.loads(requests.get(result["result"][select-1]["extractor"]+"&auth="+api_key).text)
+    print("Title: "+result["result"][select-1]["title"])
+    print("Artist: "+result["result"][select-1]["artist"])
+    print("Album Name: "+r["result"]["album_name"])
+    print("Genre: "+r["result"]["genre"])
+    print("Language: "+r["result"]["language"])
+    print("Link Image: "+r["result"]["images"][0]["url"])
+    print("Link Stream: "+r["result"]["play_url"]["medium_play_url"])
 
 search()
